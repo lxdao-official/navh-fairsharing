ffb57d07ab1ac7985136e548ae31fea9a7a37f26
Bruce Xu <bruce.xu@lxdao.io>
2023-08-17 23:14:36 +1200

    Update name

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -6,7 +6,12 @@ const Header = () => {
   const router = useRouter()
   return (
     <header className="h-[86px] flex justify-between items-center px-[112px] bg-inherit border-b border-solid border-slate-200 fixed z-10 left-0 right-0">
-      <span className="font-bold text-xl cursor-pointer" onClick={() => router.push("/")}>Split3 (aka FairSharing)</span>
+      <span
+        className="font-bold text-xl cursor-pointer"
+        onClick={() => router.push('/')}
+      >
+        FairSharing for NAVH
+      </span>
       <ConnectButton></ConnectButton>
     </header>
   )
